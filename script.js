d5c8afb2b77649094a71e62b7fad99962629208e
Deimek <333olegovich333@gmail.com>
2025-01-18 17:34:52 +0100

    add

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //const size = 10;
-// let table = '';
+// const table = '';
 // for (let i = 1; i <= size; i++) {
-//     let row = '';
+//     const row = '';
 //     for (let j = 1; j <= size; j++) {
-//         let x = i * j;
+//         const x = i * j;
 //         row += `${x.toString().padStart(4, ' ')}`;
 //     }
 //     table += row + '\n';
@@ -11,18 +11,18 @@
 // console.log(table);
 
 const size = 10;
-let table = document.createElement('table');
+const table = document.createElement('table');
 document.body.append(table);
 
 for (let i = 1; i <= size; i++) {
 
-    let elemetsRow = document.createElement('tr');
+    const elemetsRow = document.createElement('tr');
     table.append(elemetsRow);
 
     for (let j = 1; j <= size; j++) {
-        let x = i * j;
+        const x = i * j;
 
-        let elementsCell = document.createElement('td');
+        const elementsCell = document.createElement('td');
         elementsCell.textContent = x;
         elemetsRow.append(elementsCell);
 
